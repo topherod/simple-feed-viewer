acfa1df979b7f9025538f1ab4d548afbd3bf98e1
chris <cmodriscoll@gmail.com>
2016-08-11 11:43:08 -0400

    strict mode and tabs to 2 spaces (sorry not sorry)

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 // The Simple Feed Viewer application JavaScript should live here.
 //
 (function (window) {
-    var util = window.util;
+  var util = window.util;
 
-    // The API endpoint for each feeds data
-    var API_BASE_URL = 'http://data.digg.com/api/v1/feed/trending/tweets';
+  // The API endpoint for each feeds data
+  var API_BASE_URL = 'http://data.digg.com/api/v1/feed/trending/tweets';
 
-    // An array of available feeds
-    var FEEDS = [
-        { slug: 'digg' },
-        { slug: 'david_picks' },
-        { slug: 'politics' },
-        { slug: 'design' }
-    ];
-    
-    // This is a sample API call.
-    util.ajax({
-        url: API_BASE_URL,
-        data: {
-            group: 'digg'
-        },
-        success: function (data) {
-            console.log('Success!', data);
-        },
-        error: function (responseText) {
-            console.log('Error! :(', responseText);
-        }
-    });
+  // An array of available feeds
+  var FEEDS = [
+    { slug: 'digg' },
+    { slug: 'david_picks' },
+    { slug: 'politics' },
+    { slug: 'design' }
+  ];
+  
+  // This is a sample API call.
+  util.ajax({
+    url: API_BASE_URL,
+    data: {
+        group: 'digg'
+    },
+    success: function (data) {
+        // console.log('Success!', data);
+    },
+    error: function (responseText) {
+        console.log('Error! :(', responseText);
+    }
+  });
 
 })(this);
