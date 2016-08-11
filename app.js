259d50d5ecd0de9cc00762b271e26bc9cebe3308
chris <cmodriscoll@gmail.com>
2016-08-11 11:57:22 -0400

    show feed on click

--- app.js
+++ app.js
@@ -14,32 +14,45 @@
     { slug: 'design' }
   ];
   
-  // This is a sample API call.
-  util.ajax({
-    url: API_BASE_URL,
-    data: {
-        group: 'digg'
-    },
-    success: function (data) {
+  // Wrap API call in a function that can be called
+  function getTheFeed() {
+    util.ajax({
+      url: API_BASE_URL,
+      data: {
+          group: 'digg'
+      },
+      success: function (data) {
 
-      var feedDiv = document.getElementById("feed");
-      var feedHTML = '';
-      for(i = 0; i < data.mesg.length; i++) {
-        var description = data.mesg[i].description;
-        var title = data.mesg[i].title;
-        var url = data.mesg[i].url;
-        var domain = data.mesg[i].domain;
-        
-        feedHTML += '<div class="feed-item-container"><a href="' + url + '" target="_blank"  class="feed-item feed-item-' + i + '"><h3 class="title">' + title + '</h3><p class="description">' + description + '</p></a><div class="domain-container"><a href="http://' + domain + '" target="_blank" class="domain">' + domain + '</a></div></div>';
+        var feedDiv = document.getElementById("feed");
+        var feedHTML = '';
+        for(i = 0; i < data.mesg.length; i++) {
+          var description = data.mesg[i].description;
+          var title = data.mesg[i].title;
+          var url = data.mesg[i].url;
+          var domain = data.mesg[i].domain;
+          
+          feedHTML += '<div class="feed-item-container"><a href="' + url + '" target="_blank"  class="feed-item feed-item-' + i + '"><h3 class="title">' + title + '</h3><p class="description">' + description + '</p></a><div class="domain-container"><a href="http://' + domain + '" target="_blank" class="domain">' + domain + '</a></div></div>';
 
+        }
+        // print the feed to the feed div
+        feedDiv.innerHTML = feedHTML;
+
+      },
+      error: function (responseText) {
+          console.log('Error! :(', responseText);
       }
-      // print the feed to the feed div
-      feedDiv.innerHTML = feedHTML;
+    });
+  }
 
-    },
-    error: function (responseText) {
-        console.log('Error! :(', responseText);
-    }
-  });
+  // get the menu items
+  var menuItems = document.getElementsByClassName('menu-item');
+
+  // when a menu item is clicked
+  for (var i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', function() {
+      // get the feed
+      getTheFeed();
+    }, false);
+  }
 
 })(this);
